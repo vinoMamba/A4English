8fd4bf838a0a1e90c3a87a2e552536786544f1b3
vino <vino0908@outlook.com>
2023-09-12 21:51:52 +0800

    iconify icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Suspense, lazy, useEffect } from "react"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { Loading } from "./components/Loading"
 import { useDarkMode } from "./hooks/useThemeMode"
+import IconAccessibility from '~icons/carbon/accessibility'
+import IconAccountBox from '~icons/mdi/account-box'
 
 const Blackboard = lazy(() => import("./views/Blackboard"))
 
@@ -16,7 +18,13 @@ function App() {
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route index element={<Blackboard />} />
+          <Route index element={
+            <>
+              <IconAccessibility />
+              <IconAccountBox style={{ fontSize: '2em', color: 'red' }} />
+              <Blackboard />
+            </>
+          } />
           <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
